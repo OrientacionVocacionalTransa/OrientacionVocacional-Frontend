5cba8af98292524563f9efaadd6867ba0cbe439d
Lourdes Angulo Renteria <langulor2@upao.edu.pe>
2024-11-14 04:10:07 -0500

    Creacion de pagina principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '**',
+        redirectTo: 'home'
+      },
+      {
+        path: 'home',
+        title: 'Home',
+        component: HomePageComponent
+      }
+];
