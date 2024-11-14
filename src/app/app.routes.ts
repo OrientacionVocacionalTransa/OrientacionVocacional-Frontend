c22d682d186e9037718d43a58593f68f13581f49
Lourdes Angulo Renteria <langulor2@upao.edu.pe>
2024-11-14 04:20:14 -0500

    Creacion interfaz Iniciar sesion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { LoginComponent } from './components/Auth/login/login.component';
 
 export const routes: Routes = [
+    {
+        path: 'login',
+        title: 'Iniciar Sesion',
+        component: LoginComponent
+      },
+
     {
         path: '**',
         redirectTo: 'home'
