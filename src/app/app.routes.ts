11ccc37eb0915e407150df06e31bf8c0c6bcee8e
Lourdes Angulo Renteria <langulor2@upao.edu.pe>
2024-11-14 04:26:04 -0500

    Creacion interfaz registrar usuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { LoginComponent } from './components/Auth/login/login.component';
+import { RegisterComponent } from './components/Auth/register/register.component';
 
 export const routes: Routes = [
     {
         path: 'login',
         title: 'Iniciar Sesion',
         component: LoginComponent
+      },
+
+      {
+        path: 'register',
+        title: 'Registrarse',
+        component: RegisterComponent
       },
 
     {
